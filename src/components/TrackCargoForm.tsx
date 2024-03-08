5f2dd2ea802b40232b0b9fb9c4efd55407194dce
kizitech <kizitoohani@gmail.com>
2024-03-08 20:08:59 +0100

    Update TrackCargoForm.tsx

--- src/components/TrackCargoForm.tsx
+++ src/components/TrackCargoForm.tsx
@@ -14,8 +14,8 @@ const TrackCargoForm: React.FC = () => {
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                             </svg>
                         </div>
-                        <input type="search" id="search" className="block w-full h-16 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-dark focus:border-dark" placeholder="Search" required />
-                        <button type="submit" className="text-white absolute right-2.5 top-1.5 bg-dark hover:bg-opacity-95 focus:ring-4 focus:outline-none focus:ring-dark3 font-medium rounded-full md:rounded-lg text-sm px-6 py-4 dark:bg-dark dark:hover:bg-dark dark:focus:ring-dark">Track Now</button>
+                        <input type="search" id="search" className="block w-full h-16 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-full md:rounded-lg bg-gray-50 focus:ring-dark focus:border-dark" placeholder="Search" required />
+                        <button type="submit" className="text-white absolute right-1.5 top-1.5 bg-dark hover:bg-opacity-95 focus:ring-4 focus:outline-none focus:ring-dark3 font-medium rounded-full md:rounded-lg text-sm px-6 py-4 dark:bg-dark dark:hover:bg-dark dark:focus:ring-dark">Track Now</button>
                     </div>
                 </form>
 
