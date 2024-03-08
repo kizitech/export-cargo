3feb2c0fb897021047d6c5b31ba3ef2bb31a2e53
kizitech <kizitoohani@gmail.com>
2024-03-08 19:28:48 +0100

    Update CTA.tsx

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -10,10 +10,10 @@ const CTA: React.FC = () => {
                 <div className="absolute inset-0 bg-black opacity-50"></div>
             </div>
 
-            <div className="relative z-10 flex flex-col justify-center items-center h-full text-center">
+            <div className="relative z-10 flex flex-col justify-center items-center h-full text-center px-4">
                 <h1 className="text-5xl font-bold leading-tight mb-4">We Carry Your Trust</h1>
                 <p className="text-lg text-gray-300 mb-8 w-full max-w-2xl mx-auto">CARGO SHIPPING EXPRESS: With a global network and advanced logistics solutions, our air freight services provide.</p>
-                <Link to="/contact" className="px-10 py-3 bg-dark hover:bg-opacity-95 text-white rounded transition duration-300 ease-in-out transform  hover:shadow-lg">Get Started</Link>
+                <Link to="/contact" className="px-10 py-3 bg-white hover:bg-opacity-95 text-dark rounded transition duration-300 ease-in-out transform  hover:shadow-lg">Get Started</Link>
             </div>
         </div>
 
