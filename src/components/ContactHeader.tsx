16460ce067556e91e0cfad1a7436d773753769c1
kizitech <kizitoohani@gmail.com>
2024-03-08 19:56:44 +0100

    Update ContactHeader.tsx

--- src/components/ContactHeader.tsx
+++ src/components/ContactHeader.tsx
@@ -9,7 +9,7 @@ const ContactHeader: React.FC = () => {
                 <div className="absolute inset-0 bg-black opacity-50"></div>
             </div>
 
-            <div className="relative z-10 flex flex-col justify-center items-center h-full text-center">
+            <div className="relative z-10 flex flex-col justify-center items-center h-full text-center px-4">
                 <h1 className="mb-4 text-5xl font-bold tracking-tight md:text-6xl xl:text-7xl">Contact Us</h1>
                 <p className="text-lg text-gray-300 mb-8 w-full max-w-2xl mx-auto">Connect with us effortlessly on our Contact Us page. We're here to assist you promptly with all your cargo inquiries and needs. Let's start a conversation today!</p>
             </div>
