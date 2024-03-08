9a45b4a5167a0777c6958f1124a4e9cdb4f70ab5
kizitech <kizitoohani@gmail.com>
2024-03-08 19:50:15 +0100

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,11 +7,11 @@ const HeroSection = () => {
             <div className="relative overflow-hidden bg-cover bg-no-repeat min-h-screen" style={{ backgroundPosition: '50%', backgroundImage: `url(${warehouse})`, height: '500px' }}>
                 <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsla(0,0%,0%,0.75)] bg-fixed">
                     <div className="flex flex-col gap-5 h-full items-center justify-center">
-                        <a href="#" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm bg-snow rounded-full text-dark hover:bg-gray-200 mt-10" role="alert">
+                        <a href="#" className="hidden sm:inline-flex justify-between items-center py-1 px-1 pr-4 mx-4 mb-7 text-sm bg-snow rounded-full text-dark hover:bg-gray-200 mt-10" role="alert">
                             <span className="text-xs bg-gray-700 rounded-full text-white px-4 py-1.5 mr-3">Fast Shipping</span> <span className="text-sm font-medium text-dark">Explore our latest routes and services</span>
                             <svg className="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
                         </a>
-                        <div className="px-6 text-center text-white md:px-12">
+                        <div className="px-4 text-center text-white md:px-12">
                             <h1 className="mt-2 mb-16 text-5xl font-bold tracking-tight md:text-6xl xl:text-7xl">
                                 Reliable Cargo Solutions <br /><span>for Your Export Needs</span>
                             </h1>
