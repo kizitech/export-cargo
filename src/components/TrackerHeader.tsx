be2cc519a2601044fe9fcb73be841a2b9ad312f0
kizitech <kizitoohani@gmail.com>
2024-03-08 20:03:23 +0100

    Update TrackerHeader.tsx

--- src/components/TrackerHeader.tsx
+++ src/components/TrackerHeader.tsx
@@ -9,7 +9,7 @@ const TrackerHeader: React.FC = () => {
                 <div className="absolute inset-0 bg-black opacity-50"></div>
             </div>
 
-            <div className="relative z-10 flex flex-col justify-center items-center h-full text-center">
+            <div className="relative z-10 flex flex-col justify-center items-center h-full text-center px-4">
                 <h1 className="mb-4 text-5xl font-bold tracking-tight md:text-6xl xl:text-7xl">Track Your Cargo</h1>
                 <p className="text-lg text-gray-300 mb-8 w-full max-w-2xl mx-auto">Effortlessly track your cargo from origin to destination with our intuitive tracking system. Connect with us seamlessly through our Contact Us page for any assistance you need. Your cargo's journey, simplified.</p>
             </div>
