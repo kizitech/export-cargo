aabb1184e12ff80d2888b4b63db0791aca058186
Ohani Kizito <125478867+kizitech@users.noreply.github.com>
2025-03-10 19:11:06 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,8 +24,8 @@ const Navbar = () => {
                 <div className="flex items-center">
                     <ul className='hidden md:flex gap-10 transition-all'>
                         <Link to="/home"><li className="text-dark hover:text-dark3 cursor-pointer transition-colors duration-300">Home</li></Link>
-                        <Link to="/home#about"><li className="text-dark hover:text-dark3 cursor-pointer transition-colors duration-300">About Us</li></Link>
-                        <Link to="/home#services"><li className="text-dark hover:text-dark3 cursor-pointer transition-colors duration-300">Services</li></Link>
+                        <Link to="#about"><li className="text-dark hover:text-dark3 cursor-pointer transition-colors duration-300">About Us</li></Link>
+                        <Link to="/#services"><li className="text-dark hover:text-dark3 cursor-pointer transition-colors duration-300">Services</li></Link>
                         <Link to="/contact"><li className="text-dark hover:text-dark3 cursor-pointer transition-colors duration-300">Contact</li></Link>
                     </ul>
                 </div>
